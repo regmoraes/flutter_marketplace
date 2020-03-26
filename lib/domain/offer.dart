a6d29dd14666cdc33105a316f5a18034a2d49eea
Rômulo Eduardo G. Moraes <romuloeduardo.moraes@gmail.com>
2020-03-25 21:59:42 -0300

    Remove name from offer model
    Signed-off-by: Rômulo Eduardo Garcia Moraes <romulo.moraes@stone.com.br>

--- lib/domain/offer.dart
+++ lib/domain/offer.dart
@@ -2,9 +2,8 @@ import 'package:marketplace/domain/product.dart';
 
 class Offer {
   String id;
-  String name;
   num price;
   Product product;
 
-  Offer(this.id, this.name, this.price, this.product);
+  Offer(this.id, this.price, this.product);
 }
