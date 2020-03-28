79bb633a27fdc45f7c6aece32f885c23d6afacd2
Rômulo Eduardo G. Moraes <romuloeduardo.moraes@gmail.com>
2020-03-28 19:04:53 -0300

    Add components to OfferDetailPage to visually validate BLoC state consume
    Since the UI isn't finished yet UI tests will be made at the end.
    
    Signed-off-by: Rômulo Eduardo G. Moraes <romulo.moraes@stone.com.br>

--- lib/presentation/offer_detail/offer_detail_page.dart
+++ lib/presentation/offer_detail/offer_detail_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:marketplace/domain/offer.dart';
 import 'package:marketplace/graphql/client.dart';
 import 'package:marketplace/graphql/offers_repository.dart';
+import 'package:marketplace/presentation/offer_detail/event.dart';
 import 'package:marketplace/presentation/offer_detail/offer_detail_bloc.dart';
 import 'package:marketplace/presentation/offer_detail/state.dart';
 import 'package:marketplace/presentation/offer_detail/widget.dart';
@@ -14,20 +15,46 @@ class OfferDetailPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final offerDetailBloc = OfferDetailBloc(
+        offer, OffersRepository(client.value));
+
     return Scaffold(
       appBar: AppBar(
         title: Text(offer.product.name),
       ),
-      body: BlocBuilder(
-        bloc: OfferDetailBloc(
-          offer,
-          OffersRepository(client.value),
+      body: Container(
+        color: Colors.teal,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Expanded(
+              child: BlocBuilder(
+                bloc: offerDetailBloc,
+                builder: (context, state) {
+                  if (state is OfferDetail)
+                    return buildOfferDetail(context, state.offer);
+
+                  if (state is OfferPurchase) {
+                    if (state.success)
+                      return Text("Great Purchase! Now yo u have ${state
+                          .customerBalance}");
+                    else
+                      return Text("Oh no! ${state.errorMessage}");
+                  }
+                  return Text('There was an error!');
+                },
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(64),
+              child: RaisedButton(
+                onPressed: () =>
+                    offerDetailBloc.add(PurchaseOffer(offerId: offer.id)),
+                child: Text("Purchase"),
+              ),
+            ),
+          ],
         ),
-        builder: (context, state) {
-          if (state is OfferDetail)
-            return buildOfferDetail(context, state.offer);
-          return Text('There was an error!');
-        },
       ),
     );
   }
