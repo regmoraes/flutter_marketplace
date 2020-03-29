6437c6d1081c4aca364c0e678039e0e0a66806c9
Rômulo Eduardo G. Moraes <romuloeduardo.moraes@gmail.com>
2020-03-29 20:24:31 -0300

    Create new offers page that uses the OffersBloc
    Signed-off-by: Rômulo Eduardo G. Moraes <romulo.moraes@stone.com.br>

---
+++ lib/presentation/offers_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:marketplace/main.dart';
+import 'package:marketplace/presentation/offers/widget.dart';
+
+class CustomerOffersPage extends StatefulWidget {
+  final String title;
+
+  CustomerOffersPage({Key key, this.title}) : super(key: key);
+
+  @override
+  _CustomerOffersPageState createState() => _CustomerOffersPageState();
+}
+
+class _CustomerOffersPageState extends State<CustomerOffersPage> {
+  @override
+  void initState() {
+    super.initState();
+    offersBloc.getCustomerOffers();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            StreamBuilder(
+              stream: offersBloc.customerBalanceStream,
+              builder: (context, snapshot) {
+                return snapshot.hasData
+                    ? Text("The balance is ${snapshot.data}")
+                    : CircularProgressIndicator();
+              },
+            ),
+            StreamBuilder(
+              stream: offersBloc.offersStream,
+              builder: (context, snapshot) {
+                if (snapshot.hasData && !snapshot.data.fetchingOffers) {
+                  return Expanded(
+                      child: buildOfferList(
+                          context, snapshot.data.customer.offers));
+                } else {
+                  return Container(
+                    child: Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                  );
+                }
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    offersBloc.close();
+  }
+}
